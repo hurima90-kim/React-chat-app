2a8ea9918e201999db7d7758c9e31a493dadd844
hurima90-kim <hurima90@gmail.com>
2021-08-15 22:00:28 +0900

    Feat: Settin styled-component & styled-reset

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { GlobalStyles } from '../src/styles/GlobalStyles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <GlobalStyles />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>,
   document.getElementById('root')
 );
